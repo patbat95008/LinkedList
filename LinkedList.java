fb41fe088ef2bf825416d187365be6a7229e1187
Patrick <pcrussel@ucsc.edu>
2016-07-14 13:54:33 -0700

    Added Doubly Linked List
    Still coding practice (Note for the 0 people following me: I redid the
    whole thing from scratch for practice)

--- LinkedList.java
+++ LinkedList.java
@@ -1,16 +1,102 @@
+import java.util.*;
 
-public class LinkedList {
-
+class Node{
+	public Node next;
+	public Node prev;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List mainList = new List();
+	private String data;
+	
+	public Node(){
+		this.next = null;
+		this.prev = null;
 		
-		mainList.add(3);
-		mainList.add(4);
-		mainList.add(5);
-		
-		mainList.printList();
+		this.data = null;
 	}
+	
+	public Node(String data){
+		this.next = this.prev = null;
+		
+		this.data = data;
+	}
+	
+	public Node(Node next, Node prev, String data){
+		this.next = next;
+		this.prev = prev;
+		
+		this.data = data;
+	}
+	
+	public String getData(){
+		return data;
+	}
+	
+	public void setData(String data){ this.data = data;}
+}
 
+class LinkedList{
+	public Node head;
+	public Node tail;
+	
+	public LinkedList(){
+		head = tail = null;
+	}
+	
+	public void insert(String data){
+		if(this.head == null){
+			Node in = new Node(null, null, data);
+			head = tail = in;
+		}else{
+			Node nxt = new Node(data);
+			tail.next = nxt;
+			nxt.prev = tail;
+			tail = nxt;
+		}
+	}
+	
+	public void delete(String deleteMe){
+		Node index = this.head;
+		while(index != null){
+			if( deleteMe.equals( index.getData() ) ){
+				if(index != head && index != tail){
+					index.prev.next = index.next;
+					index.next.prev = index.prev;
+					break;
+				}else if(index == head){
+					head = head.next;
+					break;
+				}else if(index == tail){
+					tail = tail.prev;
+					break;
+				}
+			}
+			index = index.next;
+		}
+		
+		throw new IndexOutOfBoundsException("No " + deleteMe + " found in list\n");
+	}
+	
+	public void printList(){
+		Node index = this.head;
+		while(index != null){
+			System.out.println(index.getData());
+			index = index.next;
+		}
+	}
+	
+	public static void main(String[] args){
+		LinkedList list = new LinkedList();
+		
+		list.insert("LOL");
+		list.insert("JEJ");
+		list.insert("foo");
+		
+		list.printList();
+		System.out.printf("\n -- \n");
+		try{
+			list.delete("f");
+		}catch(IndexOutOfBoundsException e){
+			System.out.println("404 item not found");
+		}
+		list.printList();
+	}
 }
